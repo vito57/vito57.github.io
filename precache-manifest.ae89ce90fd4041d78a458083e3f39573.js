7c29fcb9aef1f621c7006f5276fe2e1eaa333565
Gala <gala@MBP-Gala.Dlink>
2019-09-30 20:57:34 +0300

    Updates

---
+++ precache-manifest.ae89ce90fd4041d78a458083e3f39573.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b49729ec67229e03a2c4183ba203d41",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fe746c219a3c313afbe8",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "8049a7efef9831f687b5",
+    "url": "/static/js/2.83fa4c66.chunk.js"
+  },
+  {
+    "revision": "fe746c219a3c313afbe8",
+    "url": "/static/js/main.8714dcb5.chunk.js"
+  },
+  {
+    "revision": "4870c6e3207ce7afeaff",
+    "url": "/static/js/runtime-main.f1fedbbe.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
